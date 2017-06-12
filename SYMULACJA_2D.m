7b73202beef51193c668b9f3162262944e48dfea
Daniel Hyjek <daniel.hyjek@gmail.com>
2017-06-12 10:50:40 +0200

    Poprawione przesuwanie sie symulacji

--- SYMULACJA_2D.m
+++ SYMULACJA_2D.m
@@ -7,36 +7,38 @@ z_axis = [-20, 30]; % axis limits
 for i=1:time-211 % time loop
     clf
     
-    x_axis=[x(i)-20 x(i)+30];
-    y_axis=[i i+50];
-    axis([x_axis, y_axis, z_axis])
-    
     hold on
     plot(x+2.5,y);  %road plot
     plot(x-2.5,y);  %road plot
     
-    %liczenie k¹ta nachylenia drogi i ustawienie widoku
+    %liczenie k¹ta nachylenia drogi
     kat=atan((y(i+20)-y(i))/abs((x(i+20)-x(i))))*180/pi;
     if x(i+20)>x(i)
-        kat=90-kat
+        kat=90-kat;
     else
-        kat=kat-90
+        kat=kat-90;
     end
-    view([-kat,1])
-    camzoom(4)  %zoom srodka plota
-%     xlabel('x'), ylabel('y'), zlabel('z')
+    
+    %obliczenie zakresu osi
+    dx=kat*0.5;
+    x_axis=[x(i)-25+dx x(i)+25+dx];
+    y_axis=[i i+50];
+    axis([x_axis, y_axis, z_axis])
+    
+    view([-kat,1])  %ustawienie k¹ta patrzenia
+    camzoom(4)      %zoom srodka plota
+
     for j=1:size(object,2) % Loop to display every object
         if (object(j).visibility(i) == 1) 
             plotObj(object(j), i)
         end
-    end    
-    if i==2
-        pause(55)
     end
     
-    %line([-1.3 -1.3],y_axis), line([1.3 1.3],y_axis) % road boundaries
-    % because radar cannot see the road only higher objects
+    %Pausa na start
+    if i==2
+        pause(5)    %w sekundach
+    end
+    
 
-%     pause(0.04)
-  pause(0.05)
+  pause(0.05)   %szybkoœæ animacji
 end
